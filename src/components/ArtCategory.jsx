fa5af34d271c001481efaf19dde82b3284695344
activeagle <activespin7@gmail.com>
2023-12-12 09:26:58 +0700

    [🧱] Structure div

---
+++ src/components/ArtCategory.jsx
@@ -0,0 +1,18 @@
+const ArtCategory = () => {
+    return (
+		<div class="bookmard">
+			<div class="category">
+                <div class="icon"><img src="icons/art.svg" /></div>
+                <div class="bookmarks art">
+                    <li><a class="bm" href="https://www.behance.net/">Behance</a></li>
+                    <li><a class="bm" href="https://www.deviantart.com/">Deviantart</a></li>
+                    <li><a class="bm" href="https://www.tumblr.com/dashboard">Tumblr</a></li>
+                    <li><a class="bm" href="https://unsplash.com/">Unsplash</a></li>
+                    <li><a class="bm" href="https://in.pinterest.com/">Pinterest</a></li>
+                </div>
+			</div>
+		</div>
+    )
+}
+
+export default ArtCategory;
