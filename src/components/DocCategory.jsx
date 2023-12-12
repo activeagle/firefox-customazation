80b9e22c2050e0a74f09138a2e1fef21f3a79901
activeagle <activespin7@gmail.com>
2023-12-12 09:00:24 +0700

    [🧱] Structure div

--- src/components/DocCategory.jsx
+++ src/components/DocCategory.jsx
@@ -1,16 +1,16 @@
 const DocCategory = () => {
     return (
         <div class="bookmark">
-        <div class="category">
-            <div class="icon"><img src="icons/" /></div>
-            <div class="bookmarks doc">
-            <li><a class="bm" href="https://en.cppreference.com/w/">C++</a></li>
-            <li><a class="bm" href="https://docs.python.org/3.7/library/index.html">Python</a></li>
-            <li><a class="bm" href="https://mail.google.com/mail/u/0/h/">Mail</a></li>
-            <li><a class="bm" href="https://github.com/dbuxy218">Github</a></li>
-            <li><a class="bm" href="https://mega.nz/">Mega</a></li>
+            <div class="category">
+                <div class="icon"><img src="icons/" /></div>
+                <div class="bookmarks doc">
+                    <li><a class="bm" href="https://en.cppreference.com/w/">C++</a></li>
+                    <li><a class="bm" href="https://docs.python.org/3.7/library/index.html">Python</a></li>
+                    <li><a class="bm" href="https://mail.google.com/mail/u/0/h/">Mail</a></li>
+                    <li><a class="bm" href="https://github.com/dbuxy218">Github</a></li>
+                    <li><a class="bm" href="https://mega.nz/">Mega</a></li>
+                </div>
             </div>
-        </div>
         </div>
     )
 }
